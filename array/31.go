0643774650aaa8554415233f2f2ed57f01c47c00
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-07-08 10:28:31 +0800

    update 31

--- array/31.go
+++ array/31.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"sort"
+	// "sort"
 )
 
 // 31. 下一个排列
@@ -28,22 +28,33 @@ func main() {
 	fmt.Println(nums) // 213
 }
 
-// 法一：O(n)
 // 从右侧开始，将大数和它左侧的小数交换位置，即可获得一个更大的数
-// 为了满足增幅小的要求：大数和小数要的位置都要尽可能靠右，交换后将大数右侧的数字升序排列
+// 为了满足增幅小的要求：大数和小数要的位置都要尽可能靠右，交换后将大数位置右侧的数字升序排列
+// O(n)
 func nextPermutation(nums []int) {
 	n := len(nums)
 	if n <= 1 {
 		return
 	}
-	// 从右侧开始，找到nums[i] > nums[i-1]的组合，交换位置
-	i := len(nums) - 1
-	for ; i > 0; i-- {
-		if nums[i] > nums[i-1] {
-			nums[i], nums[i-1] = nums[i-1], nums[i]
+	// 从右侧开始，找到 nums[min] < nums[min+1] 的组合，nums[min]就是那个小数
+	min := n - 2
+	for ; min >= 0; min-- {
+		if nums[min] < nums[min+1] {
 			break
 		}
 	}
-	// nums[i]右侧的元素是降序排列的
-	//
+	// 如果nums是降序序列，是没有比它更大的排列的，直接reverse整个数组
+	// 如果nums不是降序序列，在nums[min+1:]范围内找到：nums[min]<nums[k]，k是大数位置
+	if min >= 0 {
+		k := n - 1
+		for nums[k] <= nums[min] {
+			k--
+		}
+		nums[k], nums[min] = nums[min], nums[k]
+	}
+
+	// reverse nums[min+1:]
+	for x, y := min+1, n-1; x < y; x, y = x+1, y-1 {
+		nums[x], nums[y] = nums[y], nums[x]
+	}
 }
