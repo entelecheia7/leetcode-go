9f1b2212af0efc5238a10aca06c9913f6d8b8689
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-07-09 13:55:30 +0800

    update 78

--- array/78.go
+++ array/78.go
@@ -10,11 +10,10 @@ import (
 // https://leetcode-cn.com/problems/subsets/
 func main() {
 	fmt.Println(subsets([]int{1, 2, 3}))
+	fmt.Println(subsets2([]int{1, 2, 3}))
 }
 
-// 法一：暴力法，逐个生成长度为1、2、3……len(nums)的子集，复杂度过高，略
-
-// 法二：回溯
+// 法一：回溯，逐个生成长度为1、2、3……len(nums)的子集
 func subsets(nums []int) (result [][]int) {
 	if len(nums) == 0 {
 		return nil
@@ -35,4 +34,25 @@ func subsetBTHelper(nums []int, from int, cur []int, length int, result *[][]int
 	for i := from; i < len(nums); i++ {
 		subsetBTHelper(nums, i+1, append(cur, nums[i]), length, result)
 	}
+}
+
+// 法二：二进制
+// 将 nums 视为长度为 len(nums) 的二进制串 x，则 nums 的子集就是 0-n 位的 x 掩码
+// 以 length 为 3 的 nums 为例，其二进制位掩码分别为 000、001、010、011、100、101、111。
+// 通过计算其掩码的位是否为 1 (i >> j & 1)来决定是否添加 nums 对于位置的数字到 tmp，得出子集。
+func subsets2(nums []int) (result [][]int) {
+	if len(nums) == 0 {
+		return nil
+	}
+	for i := 0; i < (1 << len(nums)); i++ { // 本层循环用于提供掩码
+		tmp := []int{}
+		for j := 0; j < len(nums); j++ {
+			if (i >> j & 1) == 1 {
+				tmp = append(tmp, nums[j])
+			}
+		}
+		result = append(result, tmp)
+	}
+
+	return result
 }
