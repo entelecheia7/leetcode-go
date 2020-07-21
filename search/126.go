3979be0888089eeba28f1effb0c260245b84e059
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-07-21 17:03:27 +0800

    u

--- search/126.go
+++ search/126.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // 126. 单词接龙 II
@@ -20,24 +21,26 @@ func main() {
 	// 	["hit","hot","dot","dog","cog"],
 	// 	["hit","hot","lot","log","cog"]
 	// ]
-	// fmt.Println(findLadders("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}))
+	// fmt.Println(findLadders2("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}))
 
 	// fmt.Println(findLadders("a", "c", []string{"a", "b", "c"}))
 
 	// [["red","ted","tad","tax"],["red","ted","tex","tax"],["red","rex","tex","tax"]]
-	// fmt.Println(findLadders("red", "tax", []string{"ted", "tex", "red", "tax", "tad", "den", "rex", "pee"}))
+	fmt.Println(findLadders2("red", "tax", []string{"ted", "tex", "red", "tax", "tad", "den", "rex", "pee"}))
 
-	fmt.Println(findLadders("qa", "sq", []string{"si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"}))
+	// fmt.Println(findLadders("qa", "sq", []string{"si", "go", "se", "cm", "so", "ph", "mt", "db", "mb", "sb", "kr", "ln", "tm", "le", "av", "sm", "ar", "ci", "ca", "br", "ti", "ba", "to", "ra", "fa", "yo", "ow", "sn", "ya", "cr", "po", "fe", "ho", "ma", "re", "or", "rn", "au", "ur", "rh", "sr", "tc", "lt", "lo", "as", "fr", "nb", "yb", "if", "pb", "ge", "th", "pm", "rb", "sh", "co", "ga", "li", "ha", "hz", "no", "bi", "di", "hi", "qa", "pi", "os", "uh", "wm", "an", "me", "mo", "na", "la", "st", "er", "sc", "ne", "mn", "mi", "am", "ex", "pt", "io", "be", "fm", "ta", "tb", "ni", "mr", "pa", "he", "lr", "sq", "ye"}))
 }
 
-// 法一：BFS
+// 法一：单向BFS
+// 构建了一个图，在图的基础上进行BFS，queue中保存转换路径
+// 代码复杂度高，时间和空间复杂度较差
 func findLadders(beginWord string, endWord string, wordList []string) (result [][]string) {
 	wordN := len(wordList)
 	if wordN == 0 {
 		return nil
 	}
 	n := len(beginWord)
-	wordListMap := make(map[string]int, wordN)
+	wordListMap := make(map[string]int, wordN) // key是string，value是字符串在wordList的下标
 	for key, word := range wordList {
 		wordListMap[word] = key
 	}
@@ -59,59 +62,41 @@ func findLadders(beginWord string, endWord string, wordList []string) (result []
 			}
 		}
 	}
-	// fmt.Println("graph:", graph, wordListMap)
 
-	path := map[int][][]int{wordListMap[beginWord]: [][]int{{wordListMap[beginWord]}}} // 保存从start到key的可能路径
-	queue := []int{wordListMap[beginWord]}
-	visited := make(map[int]bool)
-	addToQueue := map[int]bool{wordListMap[beginWord]: true}
-	level := 0
-Loop:
+	queue := [][]string{{beginWord}} // queue保存的是从startword扩散的路径
+	cost := make([]int, wordN)       // 记录从startword转为wordList[i]的最短长度
+	for i := 0; i < wordN; i++ {
+		cost[i] = math.MaxInt64
+	}
+	cost[wordListMap[beginWord]] = 0
+
 	for len(queue) > 0 {
-		level++
 		size := len(queue)
-		fmt.Println("queue:", len(queue))
 		for i := 0; i < size; i++ {
 			cur := queue[i]
-			if visited[cur] {
-				continue
-			}
-			if cur == wordListMap[endWord] {
-				break Loop
-			}
-			visited[cur] = true
-			next := graph[queue[i]]
-			for _, p := range path[cur] { // previous path
-				for _, n := range next { // next path
-					if visited[n] {
-						continue
-					}
-					tmp := make([]int, len(p)+1)
-					copy(tmp, p)
-					tmp[len(p)] = n
-					path[n] = append(path[n], tmp)
-					if !addToQueue[n] {
-						queue = append(queue, n)
-						addToQueue[n] = true
+			last := cur[len(cur)-1]
+			if last == endWord {
+				// add to result
+				tmp := make([]string, len(cur))
+				copy(tmp, cur)
+				result = append(result, tmp)
+			} else {
+				for _, next := range graph[wordListMap[last]] {
+					if cost[next] >= cost[wordListMap[last]]+1 { // 保证转换路径不回退
+						cost[next] = cost[wordListMap[last]] + 1
+						tmp := make([]string, len(cur)+1)
+						copy(tmp, cur)
+						tmp[len(cur)] = wordList[next]
+						queue = append(queue, tmp)
 					}
 				}
 			}
 		}
-		printPath(wordList, path)
-		queue = queue[size:]
-	}
-
-	if len(path[wordListMap[endWord]]) == 0 {
-		return nil
-	}
-	for _, p := range path[wordListMap[endWord]] {
-		if len(p) == level {
-			tmp := make([]string, len(p))
-			for i := 0; i < len(p); i++ {
-				tmp[i] = wordList[p[i]]
-			}
-			result = append(result, tmp)
+		if len(result) > 0 {
+			break
 		}
+
+		queue = queue[size:]
 	}
 
 	return result
@@ -120,33 +105,96 @@ Loop:
 // 检查从start是否可以更改1个字母转换为end
 // n是字符串长度
 func checkConvertion(start, end string, n int) bool {
-	var k byte
-	word := []byte(start)
 	for i := 0; i < n; i++ {
-		old := word[i]
-		for k = 'a'; k <= 'z'; k++ {
-			if k != old {
-				word[i] = k
-				if string(word) == end {
-					return true
-				}
-			}
+		if start[i] != end[i] {
+			return start[i+1:] == end[i+1:]
 		}
-		word[i] = old
 	}
-	return false
+	return false // start == end
 }
 
-func printPath(wordList []string, path map[int][][]int) {
-	s := ""
-	for k, p := range path {
-		s += fmt.Sprintf("%s: ", wordList[k])
-		for _, v := range p {
-			for i := 0; i < len(v); i++ {
-				s += fmt.Sprintf("%s-", wordList[v[i]])
+// 法二：双向BFS
+func findLadders2(beginWord string, endWord string, wordList []string) (result [][]string) {
+	if len(wordList) == 0 {
+		return nil
+	}
+	wordListMap := make(map[string]bool, len(wordList)) // 一个全局的未访问元素数组
+	for _, w := range wordList {
+		wordListMap[w] = true
+	}
+	if !wordListMap[endWord] {
+		return nil
+	}
+	delete(wordListMap, endWord)
+
+	// queue保存这一层的节点
+	queue, queueFromEnd := map[string]bool{beginWord: true}, map[string]bool{endWord: true}
+	n := len(beginWord)
+	var j byte
+	endFlag, reverseFlag := false, false
+	path := make(map[string][]string) // 记录key可以转换的单次
+
+	for len(queue) > 0 && len(queueFromEnd) > 0 && !endFlag {
+		if len(queue) > len(queueFromEnd) {
+			queue, queueFromEnd = queueFromEnd, queue
+			reverseFlag = !reverseFlag
+		}
+		for w := range queue {
+			delete(wordListMap, w)
+		}
+		newqueue := make(map[string]bool)
+		for word := range queue {
+			tmp := []byte(word)
+			for i := 0; i < n; i++ {
+				old := tmp[i]
+				for j = 'a'; j <= 'z'; j++ {
+					if j != old {
+						tmp[i] = j
+						convertion := string(tmp)
+						if queueFromEnd[convertion] { // 双向BFS相遇
+							if reverseFlag {
+								path[convertion] = append(path[convertion], word)
+							} else {
+								path[word] = append(path[word], convertion)
+							}
+							endFlag = true
+						} else if wordListMap[convertion] { // 未访问过，说明到达下一层
+							newqueue[convertion] = true
+							if reverseFlag {
+								path[convertion] = append(path[convertion], word)
+							} else {
+								path[word] = append(path[word], convertion)
+							}
+						}
+					}
+				}
+				tmp[i] = old
 			}
-			s += fmt.Sprintf("、")
+		}
+		queue = newqueue
+	}
+	fmt.Println(path)
+	// DFS，从beginWord开始组装结果
+	cur := []string{beginWord}
+
+	var dfs func([]string)
+	dfs = func(words []string) {
+		next := path[beginWord]
+		if len(next) > 0 {
+			for _, n := range next {
+				cur = append(cur, n)
+				if n == endWord {
+					tmp := make([]string, len(cur))
+					copy(tmp, cur)
+					result = append(result, tmp)
+				} else {
+					dfs(path[n])
+				}
+				cur = cur[:len(cur)-1]
+			}
 		}
 	}
-	fmt.Println(s + "\n=====\n")
+	dfs(path[beginWord])
+
+	return result
 }
