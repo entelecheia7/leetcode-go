6bbc5d222779c25652c2f46ae697df1fff53921e
entelecheia7 <24566872+entelecheia7@users.noreply.github.com>
2020-08-13 16:55:45 +0800

    u

--- search/773.go
+++ search/773.go
@@ -14,6 +14,7 @@ import (
 //     board[i][j] 是一个 [0, 1, 2, 3, 4, 5] 的排列.
 // https://leetcode-cn.com/problems/sliding-puzzle
 func main() {
+
 	fmt.Println(slidingPuzzle([][]int{
 		{1, 2, 3},
 		{5, 4, 0}})) //-1
